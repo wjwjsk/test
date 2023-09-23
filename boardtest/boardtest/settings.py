873020f4e0a764965c412982928fc09acdf8b704
park <wjwjsk12345@gmail.com>
2023-09-23 23:31:20 +0900

    study apply

--- boardtest/boardtest/settings.py
+++ boardtest/boardtest/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -114,9 +113,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = ""
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = ""
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
